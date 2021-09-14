b25487528c364bd8fe81dfff264e6d95ebb468e8
Kayla Dombrosky <kdombroskyy@live.com>
2021-09-14 16:55:53 -0400

    save data to global store

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+// instantiate global state object
+const StoreContext = createContext(); //create new Context object 
+// pull out Provider component to make the passed state data available 
+// as a prop to all other components
+const { Provider } = StoreContext; 
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: '',
+    });
+    // use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
